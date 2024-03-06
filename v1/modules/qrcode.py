baacd4ee86f5b875bd869ab3a294bceea1b8fab0
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-06 12:54:51 +0100

    Update qrcode.py

--- v1/modules/qrcode.py
+++ v1/modules/qrcode.py
@@ -1,3 +1,17 @@
 # importamos las librerías necesarias
 import os, sys, socket, argparse, requests
 from colorama import Fore, Back
+
+# hacemos los colores de colorama
+GREEN = Fore.WHITE + Back.GREEN
+RESET = Fore.RESET + Back.RESET
+
+# creamos los argumentos de parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--qrcode', '-q',
+                    required=True,
+                    help="Indica la página de la que hacer el código QR")
+
+args = parser.parse_args()
+
