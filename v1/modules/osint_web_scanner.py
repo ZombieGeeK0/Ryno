eadc936a467e36964a53e644a7349ce8b5a4f151
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-06 15:58:12 +0100

    Update osint_web_scanner.py

--- v1/modules/osint_web_scanner.py
+++ v1/modules/osint_web_scanner.py
@@ -7,6 +7,10 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--osint', '-o',
                     required=True,
+                    help="Indica la web a la que hacer las requests")
+
+parser.add_argument('--text', '-t',
+                    required=True,
                     help="Indica el nombre de usuario a buscar")
 
 args = parser.parse_args()
@@ -20,17 +24,15 @@ def main(url):
     response = requests.get(url)
  
     if response.status_code == 200:
-        text = response.text
-        print(text)
+        plain_text = response.text
+        # print(text)
       
-        if args.text in text:
-          pass
+        if args.text in plain_text:
+          print(f'{GREEN}[INFO]:{RESET} Nombre de usuario {args.text} ha sido encontrado')
 
     else:
         print(f'\n{GREEN}[INFO]:{RESET} Error: El código de error de la página indicada no es válido.\n')
         sys.exit()
 
 # ejecutamos la función con el email indicado en el argumento
-main(args.research)
-
-# https://www.youtube.com/watch?v=2TyaTh1QRPw
+main(args.osint)
