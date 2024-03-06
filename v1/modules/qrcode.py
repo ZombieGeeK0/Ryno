41ca77f6791a6bb46cb0120248525f924ccdf3e1
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-06 13:17:22 +0100

    Update qrcode.py

--- v1/modules/qrcode.py
+++ v1/modules/qrcode.py
@@ -15,5 +15,31 @@ parser.add_argument('--qrcode', '-q',
 
 args = parser.parse_args()
 
-# https://www.google.com/search?channel=fs&client=ubuntu&q=generar+qr+con+python
-# guardar en una imagen como output.png
+def generate_qrcode(url,
+              version=4,
+              box_size=10,
+              border=4,
+              fill_color=(12, 52, 127),
+              back_color=(187, 202, 229),
+              name='qrcode_output.png'):
+                
+  qr = qrcode.QRCode(version=version,
+                     error_correction=qrcode.constants.ERROR_CORRECT_L,
+                     box_size=box_size,
+                     border=border)
+                
+  # Adjuntamos el texto o url que se desea vincular
+  qr.add_data(url)
+  qr.make(fit=True)
+                
+  # Creamos la imagen del QR Code con colores RGB
+  qr_img = qr.make_image(fill_color=fill_color, back_color=back_color)
+                
+  # Guardamos el QR Code generado
+  qr_img.save(name)
+
+# definimos la vriable url
+url = args.qrcode
+
+# ejecutamos la función
+generate_qrcode(url=url)
