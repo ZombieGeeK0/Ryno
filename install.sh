cbf52380f27319eb5e1b2a0d2d2358099c27bf15
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-06 12:52:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-clear && sudo apt-get update && sudo apt-get python3 && sudo apt-get install python3-pip && pip3 install socket && pip3 install colorama && pip3 install requests
+clear && sudo apt-get update && sudo apt-get python3 && sudo apt-get install python3-pip && pip3 install socket && pip3 install colorama && pip3 install requests && pip3 install qrcode
