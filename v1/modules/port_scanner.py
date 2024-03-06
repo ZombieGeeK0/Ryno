23a1d6e23ed7afe35ab29c7e64dd1c3b625d3b30
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-06 21:09:56 +0100

    Create port_scanner.py

---
+++ v1/modules/port_scanner.py
@@ -0,0 +1,35 @@
+# importamos las librerías necesarias
+import socket, time
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# asignamos el objetivo
+target = args.argument
+ 
+# next line gives us the ip address
+# of the target
+target_ip = socket.gethostbyname(target)
+print('Starting scan on host:', target_ip)
+ 
+# function for scanning ports
+ 
+ 
+def port_scan(port):
+    try:
+        s.connect((target_ip, port))
+        return True
+    except:
+        return False
+ 
+ 
+start = time.time()
+ 
+# here we are scanning port 0 to 4
+for port in range(5):
+    if port_scan(port):
+        print(f'port {port} is open')
+    else:
+        print(f'port {port} is closed')
+ 
+end = time.time()
+print(f'Time taken {end-start:.2f} seconds')
