ff501628ebfedf88b0909162715c2b0b8dd9c379
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-07 12:50:05 +0100

    Update decrypt_simplecrypt.py

--- v1/modules/decrypt_simplecrypt.py
+++ v1/modules/decrypt_simplecrypt.py
@@ -1,1 +1,31 @@
+# import the necesary libraries
+import argparse
+from colorama import Fore, Back
+from simplecrypt import encrypt, decrypt
 
+# create the colorama colors
+GREEN = Fore.WHITE + Back.GREEN
+RESET = Fore.RESET + Back.RESET
+
+# create the parser arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--encrypt', '-e',
+                    required=True,
+                    help="Indicates the encrypt algorithm to use")
+
+parser.add_argument('--text', '-t',
+                    required=True,
+                    help="Indicates the text")
+
+args = parser.parse_args()
+
+# create the passkey
+passkey = args.encypt
+
+# the text to encrypt
+str1 = args.text
+
+# encrypt the text
+cipher = encrypt(passkey, str1)
+print(f'\n[{GREEN}INFO{RESET}]: Encrypted text: {cipher}')
