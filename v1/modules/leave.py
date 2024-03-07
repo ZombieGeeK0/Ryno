c0c7b5bcbc60fa71a3552316421f4d9532b35a97
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-07 08:01:59 -0500

    Update leave.py

--- v1/modules/leave.py
+++ v1/modules/leave.py
@@ -4,12 +4,12 @@ import os, sys, argparse
 # create the parser arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--exit', '-e',
+parser.add_argument('--leave', '-l',
                     required=True,
                     help="Indicates the time to exit the terminal")
 
 args = parser.parse_args()
 
 # close the terminal
-sys.exit(args.exit)
+sys.exit(args.leave)
 os.system('clear && exit')
