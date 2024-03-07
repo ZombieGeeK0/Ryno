dc5bc7680eaf3a3aff7674f9ea70af82e9902a9e
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-07 13:45:54 +0100

    Create console.py

---
+++ v1/modules/console.py
@@ -0,0 +1,9 @@
+# import the necesary libraries
+import sys
+from colorama import Fore, Back
+
+# create the colorama colors
+GREEN = Fore.WHITE + Back.GREEN
+RESET = Fore.RESET + Back.RESET
+
+print(f'\n[{GREEN}INFO{RESET}]: This module is under construction...\n')
