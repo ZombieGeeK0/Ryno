6f84480b7849ae3e0e98581026822304d01f41aa
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-07 13:43:57 +0100

    Update ryno.py

--- v1/ryno.py
+++ v1/ryno.py
@@ -1,21 +1,9 @@
 '''
 Modules: decrypt.py, email.py, encrypt.py, leave.py, osint_web_scanner.py, port_scanner.py, qrcode.py
-Arguments:
-
-[-c, --console]  Requests an interactive shell. It is accompanied by the time in which to open the console in seconds (SECOND VERSION, under construction)
-[-s, --scan]  Scan the ports of the IP indicated next to the argument
-[-l, --leave]  Exits the terminal and/or program
-[-t, --text]  Provides information of some kind in plain text   
-[-e, --encrypt]  Is followed by the passkey and the plaintext subsequently specified with the "-t" parameter. Specifies the password with which to encrypt a string
-[-u, --unhash]  Breaks the hash on the encryption cipher specified after the "-u" parameter. The hashed plaintext is indicated by the parameter "-t". Indicates the passkey to decrypt the text
-[-r, --research]  Search for an email on OSINT pages. The email is indicated after the "-r" parameter
-[-q, --qrcode]  Generates a QR code of a given page after the argument "-q"
-[-o, --osint]  Scan a web with OSINT techniques
-
 '''
 
 '''
-©2024 By ZombieGeek0, MIT License.
+©2024 By ZombieGeek0, MIT License (https://github.com/ZombieGeeK0/Ryno?tab=MIT-1-ov-file).
 '''
 
 # import the necesary libraries
@@ -25,8 +13,42 @@ from colorama import Fore, Back
 # create the parser arguments
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--console', '-c',
+                    required=False,
+                    help="Requests an interactive shell")
+
+parser.add_argument('--scan', '-s',
+                    required=False,
+                    help="Scan the ports of the IP indicated next to the argument")
+
+parser.add_argument('--leave', '-l',
+                    required=False,
+                    help="Exits the terminal and/or program")
+
+parser.add_argument('--text', '-t',
+                    required=False,
+                    help="Provides information of some kind in plain text")
+
+parser.add_argument('--encrypt', '-e',
+                    required=False,
+                    help="Specifies the passkey with which to encrypt a string")
+
+parser.add_argument('--unhash', '-u',
+                    required=False,
+                    help="Indicates the passkey to decrypt the text")
+
 parser.add_argument('--research', '-r',
-                    required=True,
-                    help="Indicate the e-mail address to search")
+                    required=False,
+                    help="Search for an email on OSINT pages")
+
+parser.add_argument('--qrcode', '-q',
+                    required=False,
+                    help=" Generates a QR code of a given page after the argument '-q'")
+
+parser.add_argument('--osint', '-o',
+                    required=False,
+                    help="Scan a web with OSINT techniques")
 
 args = parser.parse_args()
+
+if args.console
