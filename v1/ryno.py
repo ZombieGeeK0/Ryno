a40b74b4b13257c096e7f8c02d51f431268710fe
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-07 13:35:31 +0100

    Update ryno.py

--- v1/ryno.py
+++ v1/ryno.py
@@ -1,3 +1,15 @@
 '''
-Modules: qrcode.py, email.py, port_scanner.py, osint_web_scanner.py
+Modules: decrypt.py, email.py, encrypt.py, leave.py, osint_web_scanner.py, port_scanner.py, qrcode.py
+Arguments:
+
+[-c, --console]  Requests an interactive shell. It is accompanied by the time in which to open the console in seconds (SECOND VERSION, under construction)
+[-s, --scan]  Scan the ports of the IP indicated next to the argument
+[-l, --leave]  Exits the terminal and/or program
+[-t, --text]  Provides information of some kind in plain text   
+[-e, --encrypt]  Is followed by the passkey and the plaintext subsequently specified with the "-t" parameter. Specifies the password with which to encrypt a string
+[-u, --unhash]  Breaks the hash on the encryption cipher specified after the "-u" parameter. The hashed plaintext is indicated by the parameter "-t". Indicates the passkey to decrypt the text
+[-r, --research]  Search for an email on OSINT pages. The email is indicated after the "-r" parameter
+[-q, --qrcode]  Generates a QR code of a given page after the argument "-q"
+[-o, --osint]  Scan a web with OSINT techniques
+
 '''
